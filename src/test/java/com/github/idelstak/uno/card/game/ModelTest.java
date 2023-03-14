cc671d62f252e6e334f8d19a50c4ba30eded7921
Hiram K <hiram.kamau@outlook.com>
2023-03-14 18:01:24 +0300

    Update ModelTest.java

--- src/test/java/com/github/idelstak/uno/card/game/ModelTest.java
+++ src/test/java/com/github/idelstak/uno/card/game/ModelTest.java
@@ -1,8 +1,11 @@
 package com.github.idelstak.uno.card.game;
 
 import com.github.idelstak.uno.card.game.Card.Colored;
+import com.github.idelstak.uno.card.game.NumberedCard.Numbered;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.Test;
@@ -27,7 +30,34 @@ public class ModelTest {
                 .sorted(Comparator.comparing(Colored::name))
                 .toList();
 
-        assertThat(numberCardsColorMap, hasEntry(0, colorsList));
+        assertThat(numberCardsColorMap, hasEntry(Numbered.ZERO.getNumber(), colorsList));
+    }
+
+    @Test
+    public void a_new_deck_contains_2_cards_numbered_1_to_9_for_each_color_except_black() {
+        var deck = new Deck();
+        var numberCardsColorMap = deck.getNumberedCardsColorMap();
+        var coloredsList = Arrays.asList(Colored.values());
+        var coloredsStream = Stream.concat(coloredsList.stream(), coloredsList.stream());
+        var doubleColorsList = coloredsStream
+                .filter(colored -> colored != Colored.BLACK)
+                .sorted(Comparator.comparing(Colored::name))
+                .toList();
+
+        assertThat(
+                numberCardsColorMap,
+                allOf(
+                        hasEntry(Numbered.ONE.getNumber(), doubleColorsList),
+                        hasEntry(Numbered.TWO.getNumber(), doubleColorsList),
+                        hasEntry(Numbered.THREE.getNumber(), doubleColorsList),
+                        hasEntry(Numbered.FOUR.getNumber(), doubleColorsList),
+                        hasEntry(Numbered.FIVE.getNumber(), doubleColorsList),
+                        hasEntry(Numbered.SIX.getNumber(), doubleColorsList),
+                        hasEntry(Numbered.SEVEN.getNumber(), doubleColorsList),
+                        hasEntry(Numbered.EIGHT.getNumber(), doubleColorsList),
+                        hasEntry(Numbered.NINE.getNumber(), doubleColorsList)
+                )
+        );
     }
 
 }
